3261c13638393d49681afd4dec3312044a465e5d
Andrey Borodulin <borodulin@gmail.com>
2015-11-20 10:19:47 +0600

    fix jquery dependence

--- JVectorMapAsset.php
+++ JVectorMapAsset.php
@@ -4,43 +4,44 @@
  * @copyright Copyright (c) 2015 Andrey Borodulin
  * @license https://github.com/borodulin/yii2-jvectormap/blob/master/LICENSE
  */
+
 namespace conquer\jvectormap;
+
 /**
  * @link http://jvectormap.com/
  */
 class JVectorMapAsset extends \yii\web\AssetBundle
 {
-	// The files are not web directory accessible, therefore we need
-	// to specify the sourcePath property. Notice the @bower alias used.
-	// public $sourcePath = '@bower/jvectormap';
-	
-	private static $_maps=[];
-	
-	public $css=[
-			'jquery-jvectormap-2.0.2.css',
-	];
-	
-	public $js=[
-			'jquery-jvectormap-2.0.2.min.js',
-	];
+    private static $_maps=[];
+    
+    public $css = [
+            'jquery-jvectormap-2.0.2.css',
+    ];
+    
+    public $js = [
+            'jquery-jvectormap-2.0.2.min.js',
+    ];
 
-	public function init()
-	{
-		$this->sourcePath=dirname(__FILE__).\DIRECTORY_SEPARATOR.'assets';
-		parent::init();
-	}
-	
-	public function registerAssetFiles($view)
-	{
-		foreach (self::$_maps as $map){
-			$this->js[]="maps/jquery-jvectormap-$map.js";
-		}
-		parent::registerAssetFiles($view);
-	}
-	
-	public static function registerMap($map)
-	{
-		self::$_maps[]=$map;
-	} 
-	
+    public $depends = [
+            'yii\web\JqueryAsset',
+    ];
+    
+    public function init()
+    {
+        $this->sourcePath = dirname(__FILE__) . '\assets';
+        parent::init();
+    }
+    
+    public function registerAssetFiles($view)
+    {
+        foreach (self::$_maps as $map){
+            $this->js[] = "maps/jquery-jvectormap-$map.js";
+        }
+        parent::registerAssetFiles($view);
+    }
+    
+    public static function registerMap($map)
+    {
+        self::$_maps[$map] = $map;
+    } 
 }
--- JVectorMapWidget.php
+++ JVectorMapWidget.php
@@ -4,61 +4,74 @@
  * @copyright Copyright (c) 2015 Andrey Borodulin
  * @license https://github.com/borodulin/yii2-jvectormap/blob/master/LICENSE
  */
+
 namespace conquer\jvectormap;
 
 use yii\helpers\Html;
 use yii\helpers\Json;
+
 /**
  * @link http://jvectormap.com/
  */
 class JVectorMapWidget extends \yii\base\Widget
 {
 
-	/**
-	 * Map name
-	 * @var string
-	 */
-	public $map;
-	
-	/**
-	 * General Map options
-	 * @link http://jvectormap.com/documentation/javascript-api/jvm-map/
-	 * @var array()
-	 */
-	public $options;
-	
-	/**
-	 * Html attributes of placeholder
-	 * @var array()
-	 */
-	public $htmlOptions;
-	
-	/**
-	 * Initializes the widget.
-	 * If you override this method, make sure you call the parent implementation first.
-	 */
-	public function init()
-	{
-		parent::init();
-		JVectorMapAsset::register($this->view);
-		if($this->map)
-			JVectorMapAsset::registerMap($this->map);
-	}
-	
-	/**
-	 * @inheritdoc
-	 */
-	public function run()
-	{
-		$view = $this->getView();
-		$htmlOptions=$this->htmlOptions;
-		if(empty($htmlOptions['id']))
-			$htmlOptions['id'] = $this->getId();
-		$options=$this->options;
-		if($this->map)
-			$options['map']=str_replace('-', '_', $this->map);
-		$options=Json::encode($options);
-		$view->registerJs("jQuery('#{$htmlOptions['id']}').vectorMap($options);");
-		return Html::tag('div','',$htmlOptions);
-	}
+    /**
+     * Map name
+     * @var string
+     */
+    public $map;
+    
+    /**
+     * General Map options
+     * @link http://jvectormap.com/documentation/javascript-api/jvm-map/
+     * @var array
+     */
+    public $options;
+
+    /**
+     * Placeholder tag
+     * @var string
+     */
+    public $tag = 'div';
+    
+    /**
+     * Html attributes of placeholder
+     * @var array
+     */
+    public $htmlOptions;
+    
+    /**
+     * Initializes the widget.
+     * If you override this method, make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        parent::init();
+        JVectorMapAsset::register($this->view);
+        if ($this->map) {
+            JVectorMapAsset::registerMap($this->map);
+        }
+        echo Html::beginTag($this->tag, $this->htmlOptions);
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        echo Html::endTag($this->tag);
+        
+        $view = $this->getView();
+        $htmlOptions = $this->htmlOptions;
+        if (empty($htmlOptions['id'])) {
+            $htmlOptions['id'] = $this->getId();
+        }
+        $options = $this->options;
+        if ($this->map) {
+            $options['map'] = str_replace('-', '_', $this->map);
+        }
+        $options = Json::encode($options);
+        $view->registerJs("jQuery('#{$htmlOptions['id']}').vectorMap($options);");
+    }
 }
